43b309c4fcdbfc05a91bd316c5b5c4640364668d
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 13:05:13 -0400

    variables info

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+age = 23
+name = 'Guilherme'
+
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+age, name = (21, "Wanderson Fernandes")
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS', 'AM']
+
+print(BRAZILIAN_STATES)
