7c4758ddbdf6fc3553de486f33ab18519c46e18b
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 13:37:08 -0400

    logical operations

---
+++ operacoes_logicas.py
@@ -0,0 +1,18 @@
+# Tabela Verdade
+verdadeiro = True
+falso = False
+
+print('-----------------------------------------')
+print('verdadeiro and verdadeiro:', verdadeiro and verdadeiro)
+print('verdadeiro and falso:', verdadeiro and falso)
+print('falso and verdadeiro:', falso and verdadeiro)
+print('falso and falso:', falso and falso)
+print('-----------------------------------------')
+print('verdadeiro or verdadeiro:', verdadeiro or verdadeiro)
+print('verdadeiro or falso:', verdadeiro or falso)
+print('falso or verdadeiro:', falso or verdadeiro)
+print('falso or falso:', falso or falso)
+print('-----------------------------------------')
+print('not falso:', not falso)
+print('not verdadeiro:', not verdadeiro)
+print('-----------------------------------------')
