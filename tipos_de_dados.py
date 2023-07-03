88b8ff1c6b7c878f52806f17fa38c8f6e2b67efd
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 11:13:49 -0400

    types of python added

---
+++ tipos_de_dados.py
@@ -0,0 +1,8 @@
+print('Wanderson-Fer')
+print(type('Wanderson-Fer'))
+print(11 + 10 + 1000)
+print(type(11 + 10 + 1000))
+print(2.5 + 1 + 0.5)
+print(type(2.5 + 1 + 0.5))
+print(True)
+print(type(True))
