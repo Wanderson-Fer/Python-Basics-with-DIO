41cc9eccab184702ad81248e4a1044d1d0db174a
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 13:22:48 -0400

    aritimetic operations

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,20 @@
+# Adição
+print('1 + 1: ', 1 + 1)
+
+# Multiplicação
+print('1 * 1: ', 1 * 1)
+
+# Subtração
+print('1 - 1: ', 1 - 1)
+
+# Divisão
+print('5 / 2: ', 5 / 2)
+
+# Divisão inteira
+print('5 // 2: ', 5 // 2)
+
+# Módulo ou resto
+print('5 % 2: ', 5 % 2)
+
+# Potênciação
+print('5 ** 2: ', 5 ** 2)
