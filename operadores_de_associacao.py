240832173b390c2a4ebe842a05a34a89795c9462
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 13:49:21 -0400

    association operatitors

---
+++ operadores_de_associacao.py
@@ -0,0 +1,7 @@
+curso = 'Curso de Python'
+frutas = ['limão', 'uva']
+saques = [10, 200, 500]
+
+print("'Python' in curso:", 'Python' in curso)
+print("'maçã' in frutas:", 'maçã' in frutas)
+print("200 not in saques:", 200 not in saques)
