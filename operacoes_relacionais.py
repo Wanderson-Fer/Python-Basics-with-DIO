356bcb087c34f20405fed9e1d77bca9fb14eedf8
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-03 13:29:27 -0400

    relational operations

---
+++ operacoes_relacionais.py
@@ -0,0 +1,11 @@
+saldo = 450
+saque = 200
+
+print(f'saldo: {saldo}', f'saque: {saque}', sep='\n')
+
+print('saldo == saque:', saldo == saque)
+print('saldo != saque:', saldo != saque)
+print('saldo <= saque:', saldo <= saque)
+print('saldo < saque:', saldo < saque)
+print('saldo >= saque:', saldo >= saque)
+print('saldo > saque:', saldo > saque)
