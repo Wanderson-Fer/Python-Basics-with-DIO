57683d0953ae0acbdd6b45b6438e26e04628c217
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-07-07 12:34:26 -0400

    Sets adicionados

---
+++ sets.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    idades = [14, 16, 18, 14, 15, 16, 20, 17, 16, 15]
+    print('lista: ', idades)
+
+    idades_unicas = set(idades)
+    print('conjunto (valores únicos): ', idades_unicas)
